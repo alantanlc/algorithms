58f0d8f2666c3b28371a3ce2f704fcbb47ab8d3b
Alan Tan <alanwuha91@gmail.com>
2020-06-23 19:39:01 +0800

    find pairs with sum k O(N)

---
+++ pairs_with_sum_k.py
@@ -0,0 +1,22 @@
+# Example: Find all pairs with sum k within an array (assuming all distinct elements).
+
+def find_pairs_with_sum_k(arr, k):
+  # Time: O(N)
+  # Space: O(N)
+
+  if not arr:
+    return
+
+  pairs = []
+  map = {arr[0]} 
+
+  for i in arr[1:]:
+    if k-i in map:
+      pairs.append((i, k-i))
+    map.add(i)
+  return pairs
+
+if __name__ == '__main__':
+  arr = [1,2,3,4]
+  k = 5
+  print(find_pairs_with_sum_k(arr, k))
